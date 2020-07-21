39eaeb79e06edd8cddb53fc8b191c49edc31f491
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2020-07-21 11:48:39 -0400

    fix for terminal init
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- src/main/java/io/openliberty/tools/intellij/util/LibertyActionUtil.java
+++ src/main/java/io/openliberty/tools/intellij/util/LibertyActionUtil.java
@@ -12,17 +12,24 @@ public class LibertyActionUtil {
 
     /**
      * Send the given command to the given ShellTerminalWidget
+     *
      * @param widget
      * @param cmd
      */
     public static void executeCommand(ShellTerminalWidget widget, String cmd) {
-        Logger log = Logger.getInstance(LibertyActionUtil.class);;
-        widget.grabFocus();
-        TtyConnector connector = widget.getTtyConnector();
-        String enterCode = new String(widget.getTerminalStarter().getCode(KeyEvent.VK_ENTER, 0), StandardCharsets.UTF_8);
-        StringBuilder result = new StringBuilder();
-        result.append(cmd).append(enterCode);
+        Logger log = Logger.getInstance(LibertyActionUtil.class);
         try {
+            widget.grabFocus();
+            TtyConnector connector = widget.getTtyConnector();
+            if (connector == null) {
+                // new terminal, use build in execute command function
+                widget.executeCommand(cmd);
+                return;
+            }
+            // existing terminal, add a new line character and send command through connector
+            String enterCode = new String(widget.getTerminalStarter().getCode(KeyEvent.VK_ENTER, 0), StandardCharsets.UTF_8);
+            StringBuilder result = new StringBuilder();
+            result.append(cmd).append(enterCode);
             connector.write(result.toString());
         } catch (IOException ex) {
             log.error(ex.getMessage());
