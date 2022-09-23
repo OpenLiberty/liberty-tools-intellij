4cf341eb45b3422f25030c94c7d27fce0179b13c
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2022-09-23 11:44:03 -0400

    If DiagnosticSeverity is not set, default to Error
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- src/main/java/com/langserver/devtools/intellij/lsp4mp/lsp4ij/operations/diagnostics/LSPDiagnosticsToMarkers.java
+++ src/main/java/com/langserver/devtools/intellij/lsp4mp/lsp4ij/operations/diagnostics/LSPDiagnosticsToMarkers.java
@@ -102,6 +102,10 @@ public class LSPDiagnosticsToMarkers implements Consumer<PublishDiagnosticsParam
     }
 
     private Color getColor(DiagnosticSeverity severity) {
+        if (severity == null) {
+            // if not set, default to Error
+            return Color.RED;
+        }
         switch (severity) {
             case Hint:
                 return Color.GRAY;
@@ -111,6 +115,7 @@ public class LSPDiagnosticsToMarkers implements Consumer<PublishDiagnosticsParam
                 return Color.GRAY;
             case Warning:
                 return Color.YELLOW;
+
         }
         return Color.GRAY;
     }
