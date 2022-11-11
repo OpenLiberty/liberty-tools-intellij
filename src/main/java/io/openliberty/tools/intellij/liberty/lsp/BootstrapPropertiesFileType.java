514bc05d7121e95b20a37cc8f0aea99344b813ef
Evie Lau <689163+evie-lau@users.noreply.github.com>
2022-11-11 13:27:17 -0600

    Narrow activation scope for LCLS config files. (#134)

--- src/main/java/io/openliberty/tools/intellij/liberty/lsp/BootstrapPropertiesFileType.java
+++ src/main/java/io/openliberty/tools/intellij/liberty/lsp/BootstrapPropertiesFileType.java
@@ -9,22 +9,38 @@
  *******************************************************************************/
 package io.openliberty.tools.intellij.liberty.lsp;
 
-import com.intellij.icons.AllIcons;
-import com.intellij.openapi.fileTypes.LanguageFileType;
-import com.intellij.openapi.util.NlsContexts;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+
+import javax.swing.Icon;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.fileTypes.ex.FileTypeIdentifiableByVirtualFile;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.openapi.vfs.VirtualFile;
 
 /**
  * Custom file type for bootstrap.properties files
  */
-public class BootstrapPropertiesFileType extends LanguageFileType {
+public class BootstrapPropertiesFileType extends LanguageFileType implements FileTypeIdentifiableByVirtualFile {
     public static final BootstrapPropertiesFileType INSTANCE = new BootstrapPropertiesFileType();
+    public static final String BOOTSTRAP_GLOB_PATTERN = "**/{src/main/liberty/config,usr/servers/**}/bootstrap.properties";
 
     private BootstrapPropertiesFileType() {
         super(BootstrapPropertiesLanguage.INSTANCE, true);
+    }
+
+    @Override
+    public boolean isMyFileType(@NotNull VirtualFile file) {
+        Path path = Paths.get(file.getCanonicalPath());
+        final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + BOOTSTRAP_GLOB_PATTERN);
+        return matcher.matches(path);
     }
 
     @Override
