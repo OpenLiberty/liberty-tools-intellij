f7d29f0c784142ca4564ebd07bdfc6c3e31a4142
Fabio Franco <fabio.franco@ibm.com>
2022-11-11 19:56:56 -0500

    first version of maven wrapper and initial steps for gradle wrapper

--- src/main/java/io/openliberty/tools/intellij/actions/LibertyDevStartAction.java
+++ src/main/java/io/openliberty/tools/intellij/actions/LibertyDevStartAction.java
@@ -9,12 +9,33 @@
  *******************************************************************************/
 package io.openliberty.tools.intellij.actions;
 
-import io.openliberty.tools.intellij.util.Constants;
-import io.openliberty.tools.intellij.util.LibertyActionUtil;
-import io.openliberty.tools.intellij.util.LibertyProjectUtil;
-import io.openliberty.tools.intellij.util.LocalizedResourceUtil;
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.util.SystemInfo;
+import io.openliberty.tools.intellij.util.*;
+import org.jetbrains.idea.maven.config.MavenConfig;
+import org.jetbrains.idea.maven.model.MavenConstants;
+import org.jetbrains.idea.maven.project.MavenGeneralSettings;
+import org.jetbrains.idea.maven.project.MavenGeneralSettingsWatcher;
+import org.jetbrains.idea.maven.project.MavenWorkspaceSettings;
+import org.jetbrains.idea.maven.project.MavenWorkspaceSettingsComponent;
+import org.jetbrains.plugins.gradle.service.settings.GradleSystemSettingsControl;
+import org.jetbrains.plugins.gradle.settings.GradleProjectSettings;
+import org.jetbrains.plugins.gradle.settings.GradleSettings;
 import org.jetbrains.plugins.terminal.ShellTerminalWidget;
+import com.intellij.openapi.project.Project;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jetbrains.idea.maven.server.MavenServerManager;
+
+//import org.jetbrains.plugins.maven;
+//import org.jetbrains.idea.maven.server.*;
+//
 public class LibertyDevStartAction extends LibertyGeneralAction {
 
     public LibertyDevStartAction() {
@@ -27,9 +48,9 @@ public class LibertyDevStartAction extends LibertyGeneralAction {
         String startCmd = null;
 
         if (projectType.equals(Constants.LIBERTY_MAVEN_PROJECT)) {
-            startCmd = "mvn io.openliberty.tools:liberty-maven-plugin:dev";
+            startCmd = getMavenConfigPreference(project) + " io.openliberty.tools:liberty-maven-plugin:dev";
         } else if (projectType.equals(Constants.LIBERTY_GRADLE_PROJECT)) {
-            startCmd = "gradle libertyDev";
+            startCmd = getGradleConfigPreference(project) + " libertyDev";
         }
         if (widget == null) {
             LOGGER.debug("Unable to start Liberty dev mode, could not get or create terminal widget for " + projectName);
@@ -38,5 +59,51 @@ public class LibertyDevStartAction extends LibertyGeneralAction {
         String cdToProjectCmd = "cd \"" + buildFile.getParent().getCanonicalPath() + "\"";
         LibertyActionUtil.executeCommand(widget, cdToProjectCmd);
         LibertyActionUtil.executeCommand(widget, startCmd);
+    }
+
+    private String getGradleConfigPreference(Project project) {
+//        GradleSystemSettingsControl gradleSettings = GradleSystemSettingsControl;
+        GradleSettings gradleSettings = GradleSettings.getInstance(project);
+//        gradleSettings.myLinkedProjectsSettings
+        GradleProjectSettings gradleProjectSettings = gradleSettings.getLinkedProjectSettings(project.getBasePath());
+
+        return "gradle";
+    }
+
+    private String getMavenConfigPreference(Project project) {
+        MavenServerManager mavenManager = MavenServerManager.getInstance();
+        MavenGeneralSettings mavenSettings = MavenWorkspaceSettingsComponent.getInstance(project).getSettings().getGeneralSettings();
+        String mavenHome = mavenSettings.getMavenHome();
+
+        if (mavenManager.WRAPPED_MAVEN.equals(mavenHome)) {
+            // it is set to use the wrapper
+            String mvnwPath = getLocalMavenWrapper(project);
+            if (mvnwPath != null) {
+                return mvnwPath;
+            }
+        } else {
+            // try to use maven home path defined in the settings
+            File mavenHomeFile = getCustomMavenPath(mavenHome);
+            if (mavenHomeFile != null) {
+                return mavenHomeFile.getAbsolutePath();
+            }
+        }
+        // default maven
+        return "mvn";
+    }
+
+    private String getLocalMavenWrapper(Project project) {
+        String mvnw = SystemInfo.isWindows ? "mvnw.cmd" : "./mvnw";
+        File file = new File(project.getBasePath() + File.separator + mvnw);
+        return file.exists() ? mvnw : null;
+    }
+
+    private File getCustomMavenPath(String customMavenHome) {
+        File mavenHomeFile = MavenServerManager.getMavenHomeFile(customMavenHome); // when customMavenHome path is invalid it returns null
+        if (mavenHomeFile != null) {
+            File file = new File(mavenHomeFile.getAbsolutePath() + File.separator + "bin" + File.separator + "mvn");
+            return file.exists() ? file : null;
+        }
+        return null;
     }
 }
