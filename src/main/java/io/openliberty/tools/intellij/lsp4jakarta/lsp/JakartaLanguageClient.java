239057cde9aebbc9fa91fac5dd66a6dad4911288
Paul Gooderham <turkeyonmarblerye@gmail.com>
2022-12-08 11:49:31 -0500

    Use lsp4jakarta extended API to specify package and class names required for a new class snippet.
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

--- src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp/JakartaLanguageClient.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp/JakartaLanguageClient.java
@@ -12,14 +12,10 @@
 
 package io.openliberty.tools.intellij.lsp4jakarta.lsp;
 
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.libraries.Library;
-import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.search.GlobalSearchScope;
 import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.PropertiesManagerForJakarta;
 import io.openliberty.tools.intellij.lsp4mp.MicroProfileProjectService;
 import io.openliberty.tools.intellij.lsp4mp.lsp4ij.LanguageClientImpl;
@@ -27,19 +23,19 @@ import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.utils.IPsiUtils;
 import io.openliberty.tools.intellij.lsp4mp4ij.psi.internal.core.ls.PsiUtilsLSImpl;
 import org.apache.commons.lang3.tuple.Pair;
 import org.eclipse.lsp4j.PublishDiagnosticsParams;
+import org.eclipse.lsp4j.jsonrpc.CompletableFutures;
 import org.eclipse.lsp4jakarta.api.JakartaLanguageClientAPI;
 import org.eclipse.lsp4jakarta.commons.JakartaClasspathParams;
 import org.eclipse.lsp4jakarta.commons.JakartaDiagnosticsParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 /**
  * Adapted from https://github.com/redhat-developer/intellij-quarkus/blob/2585eb422beeb69631076d2c39196d6eca2f5f2e/src/main/java/com/redhat/devtools/intellij/quarkus/lsp/QuarkusLanguageClient.java
- * to start LSP4MP, Language Server for MicroProfile
+ * to match LSP4MP, Language Server for MicroProfile
  */
 public class JakartaLanguageClient extends LanguageClientImpl implements JakartaLanguageClientAPI, MicroProfileProjectService.Listener {
   private static final Logger LOGGER = LoggerFactory.getLogger(JakartaLanguageClient.class);
@@ -51,23 +47,12 @@ public class JakartaLanguageClient extends LanguageClientImpl implements Jakarta
   // Support the message "jakarta/java/classpath"
   @Override
   public CompletableFuture<List<String>> getContextBasedFilter(JakartaClasspathParams classpathParams) {
+    String uri = classpathParams.getUri();
     List<String> snippetContexts = classpathParams.getSnippetCtx();
-    // ask the Java component if the classpath of the current module contains the specified Jakarta types.
-    JavaPsiFacade javaFacade = JavaPsiFacade.getInstance(getProject());
-    GlobalSearchScope scope = GlobalSearchScope.allScope(getProject());
-    List<String> validCtx = new ArrayList<String>();
-    if (javaFacade != null && scope != null) {
-      for (String typeCtx : snippetContexts) {
-        Object type = ApplicationManager.getApplication().runReadAction((Computable<Object>) () -> javaFacade.findClass(typeCtx, scope));
-        validCtx.add(type != null ? typeCtx : null); // list will be the same size as input
-      }
-    } else {
-      // Error: none of these contexts will add to the completions
-      for (String typeCtx : snippetContexts) {
-        validCtx.add(null); // list will be the same size as input
-      }
-    }
-    return CompletableFuture.completedFuture(validCtx);
+    Project project = getProject();
+    return CompletableFutures.computeAsync((cancelChecker) -> {
+              return PropertiesManagerForJakarta.getInstance().getExistingContextsFromClassPath(uri, snippetContexts, project);
+            });
   }
 
   // Support the message "jakarta/java/diagnostics"
