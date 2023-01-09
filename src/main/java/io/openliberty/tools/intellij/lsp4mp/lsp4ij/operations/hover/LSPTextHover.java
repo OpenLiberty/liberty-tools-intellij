3bf17ef27e76e9cfe66f4f0be2ccefbffa557d4e
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2023-01-09 12:42:17 -0500

    Enable hover for server.xml files
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- src/main/java/io/openliberty/tools/intellij/lsp4mp/lsp4ij/operations/hover/LSPTextHover.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4mp/lsp4ij/operations/hover/LSPTextHover.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Red Hat, Inc.
+ * Copyright (c) 2020, 2023 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
@@ -140,11 +140,18 @@ public class LSPTextHover extends DocumentationProviderEx implements ExternalDoc
     @Nullable
     @Override
     public String generateDoc(PsiElement element, @Nullable PsiElement originalElement) {
-        Editor editor = LSPIJUtils.editorForElement(element);
+        PsiElement elem = element;
+        Editor editor = LSPIJUtils.editorForElement(elem);
+        if (editor == null) {
+            // for some files (e.g. xml) element cannot resolve the associated VirtualFile and Editor, so we try to resolve again with originalElement
+            editor = LSPIJUtils.editorForElement(originalElement);
+            elem = originalElement;
+            LOGGER.info("Cannot resolve VirtualFile and Editor for element: " + element.getText() + ". Using originalElement: " + originalElement.getText() + " for hover request.");
+        }
         if (editor != null) {
-            initiateHoverRequest(element, editor);
+            initiateHoverRequest(elem, editor);
             try {
-                String result = request.get(500, TimeUnit.MILLISECONDS).stream()
+                String result = request.get(1, TimeUnit.SECONDS).stream()
                         .filter(Objects::nonNull)
                         .map(LSPTextHover::getHoverString)
                         .filter(Objects::nonNull)
@@ -154,7 +161,8 @@ public class LSPTextHover extends DocumentationProviderEx implements ExternalDoc
                     return styleHtml(editor, RENDERER.render(PARSER.parse(result)));
                 }
             } catch (ExecutionException | TimeoutException e) {
-                LOGGER.warn(e.getLocalizedMessage(), e);
+                String fileName = elem.getContainingFile().getVirtualFile() != null ? String.valueOf(elem.getContainingFile().getVirtualFile()) : String.valueOf(elem.getContainingFile());
+                LOGGER.warn(String.format("Unable to generate documentation for %s. ", fileName) +  e.getLocalizedMessage(), e);
             } catch (InterruptedException e) {
                 LOGGER.warn(e.getLocalizedMessage(), e);
                 Thread.currentThread().interrupt();
