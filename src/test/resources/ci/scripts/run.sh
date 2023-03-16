eab5ef571ba094ef7e4949813833d610d1ce940f
Edward Mezarina <mezarina@us.ibm.com>
2023-03-16 16:22:56 -0400

    Prime the env before running tests
    Signed-off-by: Edward Mezarina <mezarina@us.ibm.com>

--- src/test/resources/ci/scripts/run.sh
+++ src/test/resources/ci/scripts/run.sh
@@ -17,6 +17,37 @@ currentTime=(date +"%Y/%m/%d-%H:%M:%S:%3N")
 # Operating system.
 OS=$(uname -s)
 
+# Primes the environment before running the tests.
+# This decreases the possibility of intermittent test issues
+# related to varying dependency download times.
+# @parameter1 The main project directory path.
+prefetchDependencies() {
+      local initDir="$1"
+
+      # Go to the init dir.
+      cd "$initDir"
+
+      # Build the product to prime for product dependencies.
+      ./gradlew build -x test
+
+      # Run through dev mode server install/create and feature installation for the Maven app.
+      cd "src/test/resources/apps/maven/single-mod-maven-app"
+      ./mvnw liberty:install-server
+      ./mvnw liberty:create
+      ./mvnw liberty:install-feature
+
+      # Run through dev mode server install/create and feature installation for the Gradle app.
+      cd "$initDir"
+      cd "src/test/resources/apps/gradle/single-mod-gradle-app"
+      ./gradlew installLiberty
+      ./gradlew libertyCreate
+      ./gradlew installFeature
+
+      # Go to the init dir.
+      cd "$initDir"
+}
+
+# Runs UI tests and collects debug data.
 main() {
     echo -e "\n$(${currentTime[@]}): INFO: Starting integration test run."
 
@@ -40,13 +71,18 @@ main() {
     # Clean the project.
     ./gradlew clean
 
-    # Build the product. This avoids dependency download related timing issues and speeds up testing.
-    ./gradlew build -x test
+    # Prime the environment.
+    prefetchDependencies "$currentLoc"
+    rc=$?
+    if [ "$rc" -ne 0 ]; then
+        echo -e "\n$(${currentTime[@]}): ERROR: Failure while priming the env. rc: ${rc}."
+        exit 11
+    fi
 
-    # Run IDEA.
+    # Start the IDE.
     ./gradlew runIdeForUiTests --info  > remoteServer.log  2>&1 &
 
-    # WAIT for the IDE to come up.
+    # Wait for the IDE to come up.
     echo -e "\n$(${currentTime[@]}): INFO: Waiting for the test IDE to start."
     callLivenessEndpoint=(curl -s http://localhost:8082)
     count=1
@@ -69,17 +105,17 @@ main() {
     if [ "$rc" -ne 0 ]; then
         echo -e "\n$(${currentTime[@]}): ERROR: Failure while running tests. rc: ${rc}."
 
-        echo -e "DEBUG: Collecting IDE remote server logs...\n"
+        echo -e "DEBUG: Gathering IDE remote server logs...\n"
         if [ -f "$currentLoc/remoteServer.log" ]; then
             cp "$currentLoc"/remoteServer.log "$currentLoc"/build/reports/.
         fi
 
-        echo -e "DEBUG: Collecting videos...\n"
+        echo -e "DEBUG: Gathering videos...\n"
         if [ -d "$currentLoc/video" ]; then
             mv "$currentLoc"/video "$currentLoc"/build/reports/.
         fi
 
-        echo -e "DEBUG: Collecting IDEA log...\n"
+        echo -e "DEBUG: Gathering the IDEA log...\n"
         if [ -f "$currentLoc/build/idea-sandbox/system/log/idea.log" ]; then
             cp "$currentLoc"/build/idea-sandbox/system/log/idea.log "$currentLoc"/build/reports/.
         fi
