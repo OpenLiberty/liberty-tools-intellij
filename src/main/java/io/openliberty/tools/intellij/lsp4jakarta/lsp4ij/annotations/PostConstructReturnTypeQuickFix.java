3efe653d124b612e6095e9786f89ccdebddd0dbc
Paul Gooderham <turkeyonmarblerye@gmail.com>
2023-04-06 14:41:58 -0400

    Add PostConstructReturnTypeQuickFix to fix @PostConstruct issue.
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

---
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/annotations/PostConstructReturnTypeQuickFix.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2022, 2023 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Yijia Jing
+ *******************************************************************************/
+package io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.annotations;
+
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiPrimitiveType;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtil;
+import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.codeAction.proposal.ModifyReturnTypeProposal;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.codeaction.JavaCodeActionContext;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.corrections.proposal.ChangeCorrectionProposal;
+import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.Diagnostic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Quick fix for AnnotationDiagnosticsCollector that changes the return type of a method to void.
+ * Uses ModifyReturnTypeProposal.
+ *
+ * @author Yijia Jing
+ *
+ */
+public class PostConstructReturnTypeQuickFix {
+    public List<? extends CodeAction> getCodeActions(JavaCodeActionContext context, Diagnostic diagnostic) {
+        List<CodeAction> codeActions = new ArrayList<>();
+        PsiElement node = context.getCoveredNode();
+        PsiMethod parentType = getBinding(node);
+        String name = "Change return type to void";
+        ChangeCorrectionProposal proposal = new ModifyReturnTypeProposal(name, context.getCompilationUnit(),
+                context.getASTRoot(), parentType, 0, PsiPrimitiveType.VOID);
+        CodeAction codeAction = context.convertToCodeAction(proposal, diagnostic);
+        codeActions.add(codeAction);
+        return codeActions;
+    }
+
+    protected PsiMethod getBinding(PsiElement node) {
+        if (node instanceof PsiMethod) {
+            return (PsiMethod) node;
+        }
+        return PsiTreeUtil.getParentOfType(node, PsiMethod.class);
+    }
+}
---
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/codeAction/proposal/ModifyReturnTypeProposal.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2022, 2023 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Yijia Jing
+ *******************************************************************************/
+package io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.codeAction.proposal;
+
+import com.intellij.openapi.editor.Document;
+import com.intellij.psi.*;
+import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.annotations.PostConstructReturnTypeQuickFix;
+import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.codeAction.JakartaCodeActionHandler;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.corrections.proposal.Change;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.corrections.proposal.ChangeCorrectionProposal;
+import org.eclipse.lsp4j.CodeActionKind;
+
+/**
+ * Code action proposal for changing the return type of a method.
+ *
+ * @author Yijia Jing
+ * @see JakartaCodeActionHandler
+ * @see PostConstructReturnTypeQuickFix
+ *
+ */
+public class ModifyReturnTypeProposal extends ChangeCorrectionProposal {
+
+    private final PsiFile invocationNode;
+    private final PsiElement binding;
+    private final PsiType newReturnType;
+
+    /**
+     * Constructor for ModifyReturnTypeProposal that accepts the new return type of a method.
+     *
+     * @param newReturnType the new return type to change to
+     */
+    public ModifyReturnTypeProposal(String label, PsiFile targetCU, PsiFile invocationNode,
+                                    PsiElement binding, int relevance, PsiType newReturnType) {
+        super(label, CodeActionKind.QuickFix, relevance);
+        this.invocationNode = invocationNode;
+        this.binding = binding;
+        this.newReturnType = newReturnType;
+    }
+
+    @SuppressWarnings("restriction")
+    @Override
+    public Change getChange() {
+        if (binding instanceof PsiMethod) {
+            PsiMethod method = ((PsiMethod) binding);
+            PsiTypeElement oldType = method.getReturnTypeElement();
+            PsiElementFactory factory = JavaPsiFacade.getInstance(binding.getProject()).getElementFactory();
+            PsiTypeElement newType = factory.createTypeElement(newReturnType);
+            if (oldType != null) {
+                oldType.replace(newType);
+            }
+        }
+        final Document changed = invocationNode.getViewProvider().getDocument();
+        return new Change(changed, changed);
+    }
+}
