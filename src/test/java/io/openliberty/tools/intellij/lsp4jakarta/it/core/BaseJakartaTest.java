e6f3f14a1c3467358072f04895730323283afe97
Michael Glavassevich <mrglavas@ca.ibm.com>
2023-05-15 14:36:09 -0400

    Removing unused Eclipse JDT code.
    Signed-off-by: Michael Glavassevich <mrglavas@ca.ibm.com>

--- src/test/java/io/openliberty/tools/intellij/lsp4jakarta/it/core/BaseJakartaTest.java
+++ src/test/java/io/openliberty/tools/intellij/lsp4jakarta/it/core/BaseJakartaTest.java
@@ -13,15 +13,6 @@
 *******************************************************************************/
 package io.openliberty.tools.intellij.lsp4jakarta.it.core;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
@@ -33,19 +24,15 @@ import com.intellij.testFramework.builders.JavaModuleFixtureBuilder;
 import com.intellij.testFramework.builders.ModuleFixtureBuilder;
 import com.intellij.testFramework.fixtures.*;
 import org.apache.commons.io.FileUtils;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.internal.core.JavaModelManager;
 import org.jetbrains.idea.maven.MavenImportingTestCase;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * Modified from:
@@ -127,65 +114,5 @@ public class BaseJakartaTest extends MavenImportingTestCase {
             setupJdkForModule(module.getName());
         }
         return module;
-    }
-
-    protected static IJavaProject loadJavaProject(String projectName, String parentDirName)
-            throws CoreException, Exception {
-        // Move project to working directory
-        File projectFolder = copyProjectToWorkingDirectory(projectName, parentDirName);
-
-        IPath path = new Path(new File(projectFolder, "/.project").getAbsolutePath());
-        IProjectDescription description = ResourcesPlugin.getWorkspace().loadProjectDescription(path);
-        IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(description.getName());
-
-        if (!project.exists()) {
-            project.create(description, null);
-            project.open(null);
-
-            // We need to call waitForBackgroundJobs with a Job which does nothing to have a
-            // resolved classpath (IJavaProject#getResolvedClasspath) when search is done.
-            IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
-                @Override
-                public void run(IProgressMonitor monitor) throws CoreException {
-                    monitor.done();
-
-                }
-            };
-            IProgressMonitor monitor = new NullProgressMonitor();
-            JavaCore.run(runnable, null, monitor);
-            waitForBackgroundJobs(monitor);
-        }
-
-        IJavaProject javaProject = JavaModelManager.getJavaModelManager().getJavaModel()
-                .getJavaProject(description.getName());
-        return javaProject;
-    }
-
-    private static File copyProjectToWorkingDirectory(String projectName, String parentDirName) throws IOException {
-        File from = new File("projects/" + parentDirName + "/" + projectName);
-        File to = new File(getWorkingProjectDirectory(),
-                java.nio.file.Paths.get(parentDirName, projectName).toString());
-
-        if (to.exists()) {
-            FileUtils.forceDelete(to);
-        }
-
-        if (from.isDirectory()) {
-            FileUtils.copyDirectory(from, to);
-        } else {
-            FileUtils.copyFile(from, to);
-        }
-
-        return to;
-    }
-
-    public static File getWorkingProjectDirectory() throws IOException {
-        File dir = new File("target", "workingProjects");
-        FileUtils.forceMkdir(dir);
-        return dir;
-    }
-
-    private static void waitForBackgroundJobs(IProgressMonitor monitor) throws Exception {
-        JobHelpers.waitForJobsToComplete(monitor);
     }
 }
