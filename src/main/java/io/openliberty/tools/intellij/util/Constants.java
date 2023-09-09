34f94aa28c81f1973a8aa1417754d7228b032384
Anusree Lakshmi <141303561+anusreelakshmi934@users.noreply.github.com>
2023-09-09 09:59:22 +0530

    Update src/main/java/io/openliberty/tools/intellij/util/Constants.java
    Co-authored-by: Trevor Crawford <TrevCraw@users.noreply.github.com>

--- src/main/java/io/openliberty/tools/intellij/util/Constants.java
+++ src/main/java/io/openliberty/tools/intellij/util/Constants.java
@@ -87,9 +87,9 @@ public final class Constants {
      */
     public static final String SERVER_ENV_GLOB_PATTERN = "**/{src/main/liberty/config,usr/servers/**}/server.env";
 
-    public static final String LIBERTY_XML_SERVER = "LibertyConfigLanguageServer";
-    public static final String LIBERTY_CONFIG_SERVER="LibertyConfigLanguageServer";
-    public static final String JAKARTA_LANG_SERVER="Jakarta Language Server";
-    public static final String MICROPROFILE_SERVER="Microprofile Server";
+    public static final String LIBERTY_XML_SERVER = "LemMinX";
+    public static final String LIBERTY_CONFIG_SERVER="Liberty Config";
+    public static final String JAKARTA_LANG_SERVER="Eclipse LSP4Jakarta";
+    public static final String MICROPROFILE_SERVER="Eclipse LSP4MP";
 
 }
