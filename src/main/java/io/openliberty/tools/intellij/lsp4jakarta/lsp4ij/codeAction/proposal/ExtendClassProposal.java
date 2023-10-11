02bbbab2ae4d3b379910632287a51704273dda46
Michael Glavassevich <mrglavas@ca.ibm.com>
2023-10-11 13:28:12 -0400

    Resolve functional issues with ExtendClassProposal. This fixes an NPE and corrects a problem with the code change computed by the quick fix.
    Signed-off-by: Michael Glavassevich <mrglavas@ca.ibm.com>

--- src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/codeAction/proposal/ExtendClassProposal.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/codeAction/proposal/ExtendClassProposal.java
@@ -14,15 +14,36 @@
  *******************************************************************************/
 package io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.codeAction.proposal;
 
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiFile;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.corrections.proposal.ASTRewriteCorrectionProposal;
 import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.corrections.proposal.ImplementInterfaceProposal;
+import org.eclipse.lsp4j.CodeActionKind;
 
-public class ExtendClassProposal extends ImplementInterfaceProposal {
+public class ExtendClassProposal extends ASTRewriteCorrectionProposal {
 
-    public ExtendClassProposal(String name, PsiFile sourceCU, PsiClass binding, PsiFile astRoot,
-                               String interfaceType, int relevance) {
-        super(null, binding, astRoot, interfaceType, relevance, sourceCU);
+    private final PsiClass fBinding;
+    private final String parentClassType;
+
+    public ExtendClassProposal(String name, PsiFile targetCU, PsiFile sourceCU,
+                               PsiClass binding, String parentClassType, int relevance) {
+        super(name, CodeActionKind.QuickFix, targetCU, relevance, sourceCU);
+        fBinding = binding;
+        this.parentClassType = parentClassType;
     }
 
+    @Override
+    public void performUpdate() {
+        final Project project = fBinding.getProject();
+        final PsiClass parentClass = JavaPsiFacade.getInstance(project).
+                findClass(parentClassType, GlobalSearchScope.allScope(project));
+        if (parentClass != null) {
+            final PsiReferenceList extendsList = fBinding.getExtendsList();
+            if (extendsList != null) {
+                extendsList.add(PsiElementFactory.getInstance(project).
+                        createClassReferenceElement(parentClass));
+            }
+        }
+    }
 }
