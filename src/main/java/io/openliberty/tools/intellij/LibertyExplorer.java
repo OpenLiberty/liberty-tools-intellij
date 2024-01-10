82c3d36aa70f9f56ffd58e50e7f7cbf03e98853a
Rahul Biju <Rahul.Biju@ibm.com>
2024-01-10 14:16:58 +0530

    removed spacing

--- src/main/java/io/openliberty/tools/intellij/LibertyExplorer.java
+++ src/main/java/io/openliberty/tools/intellij/LibertyExplorer.java
@@ -334,10 +334,8 @@ public class LibertyExplorer extends SimpleToolWindowPanel {
                 boolean leaf,
                 int row,
                 boolean hasFocus) {
-            /**
-             * modification to get tooltiptext for liberty module tree
-             */
             super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+
             // assign gear icon to action nodes
             if (leaf) {
                 setIcon(LibertyPluginIcons.IntelliJGear);
