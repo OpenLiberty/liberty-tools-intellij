05ec8358c8fefaa0be6bf554a51e149ad5fdbe52
dessina-devasia <143582034+dessina-devasia@users.noreply.github.com>
2024-03-01 19:20:45 +0530

    [#415]: Removed unused projects

--- src/test/resources/projects/maven/custom-liberty-install-maven-app/customInstallDir/wlp/lib/platform/checksums/com.ibm.websphere.appserver.logging-1.0.cs
+++
@@ -1,7 +0,0 @@
-#Mon Oct 30 15:45:41 GMT 2023
-lib/platform/defaultLogging-1.0.mf=5dca00ad3a1c72fd06670dc28717f34f
-dev/spi/ibm/javadoc/com.ibm.websphere.appserver.spi.logging_1.1-javadoc.zip=96f2059e1ab7505fdfd42d014088b5df
-lib/com.ibm.ws.collector.manager_1.0.83.jar=2be41bbae57bc1f3ea39ea964ddb4750
-dev/spi/ibm/com.ibm.websphere.appserver.spi.logging_1.1.83.jar=642623cae3877b65b1579dc060bd090a
-lib/com.ibm.ws.logging_1.0.83.jar=5413337a2a4138d6b2aaf8d751d44b0e
-lib/com.ibm.ws.logging.osgi_1.0.83.jar=9d5510d64dbc36224273f0c9100aae62
