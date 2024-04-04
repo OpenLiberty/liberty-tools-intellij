9fb1479baf4e80ddbf8c1b1fa43450b8810ce64a
Paul Gooderham <turkeyonmarblerye@gmail.com>
2024-04-04 18:31:43 -0400

    Add language server factory classes
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

---
+++ src/main/java/io/openliberty/tools/intellij/liberty/lsp/LibertyConfigLanguageServerFactory.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2024 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package io.openliberty.tools.intellij.liberty.lsp;
+
+import com.intellij.openapi.project.Project;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.lsp4mp.ls.api.MicroProfileLanguageServerAPI;
+import org.microshed.lsp4ij.LanguageServerFactory;
+import org.microshed.lsp4ij.client.LanguageClientImpl;
+import org.microshed.lsp4ij.server.StreamConnectionProvider;
+
+public class LibertyConfigLanguageServerFactory implements LanguageServerFactory {
+    @Override
+    public StreamConnectionProvider createConnectionProvider(Project project) {
+        return new LibertyConfigLanguageServer();
+    }
+
+    @Override
+    public LanguageClientImpl createLanguageClient(Project project) {
+        return new LibertyConfigLanguageClient(project);
+    }
+
+    @Override
+    public Class<? extends LanguageServer> getServerInterface() {
+        return MicroProfileLanguageServerAPI.class;
+    }
+}
