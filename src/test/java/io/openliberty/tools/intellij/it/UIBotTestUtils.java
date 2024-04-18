0395e18706fb739bf8f19692dcdf084c19de770a
anusreelakshmi934 <anusree.lakshmi@ibm.com>
2024-04-18 14:07:09 +0530

    Updated comment in openFile method

--- src/test/java/io/openliberty/tools/intellij/it/UIBotTestUtils.java
+++ src/test/java/io/openliberty/tools/intellij/it/UIBotTestUtils.java
@@ -630,7 +630,7 @@ public class UIBotTestUtils {
                 projTree.expand(filePath);
 
                 // Awaiting the full expansion of the project tree to ensure that it correctly selects the server.env file.
-                // This process may require modification in the future.
+                // We need to seek a more effective solution for this in the future.
                 TestUtils.sleepAndIgnoreException(5);
 
                 projTree.findText(fileName).doubleClick();
