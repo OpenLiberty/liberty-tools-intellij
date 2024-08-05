8fc4b85a33fa7c88be8a3c28b0deacc461ea79a9
anusreelakshmi934 <anusree.lakshmi@ibm.com>
2024-08-05 17:57:12 +0530

    Update LibertyProjectAction.java

--- src/main/java/io/openliberty/tools/intellij/actions/LibertyProjectAction.java
+++ src/main/java/io/openliberty/tools/intellij/actions/LibertyProjectAction.java
@@ -23,7 +23,6 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.*;
-import java.util.function.Function;
 
 public abstract class LibertyProjectAction extends LibertyGeneralAction {
     private static final Logger LOGGER = Logger.getInstance(LibertyProjectAction.class);
@@ -45,17 +44,22 @@ public abstract class LibertyProjectAction extends LibertyGeneralAction {
         }
         List<BuildFile> buildFileList = getBuildFileList(project);
         if (!buildFileList.isEmpty()) {
-            String[] projectName = extractBuildFileAttributes(buildFileList, BuildFile::getProjectName);
-            String[] projectPath = extractBuildFileAttributes(buildFileList, buildFile -> buildFile.getBuildFile().getPath());
+            List<Map.Entry<String, String>> attributesList = extractBuildFileAttributes(buildFileList);
+            String[] projectNames = attributesList.stream()
+                    .map(Map.Entry::getKey)
+                    .toArray(String[]::new);
+            String[] projectPaths = attributesList.stream()
+                    .map(Map.Entry::getValue)
+                    .toArray(String[]::new);
 
             LibertyProjectChooserDialog dialog = new LibertyProjectChooserDialog(
                     project,
                     getChooseDialogMessage(),
                     getChooseDialogTitle(),
                     LibertyPluginIcons.libertyIcon_40,
-                    projectName,
-                    projectPath,
-                    projectName[0]);
+                    projectNames,
+                    projectPaths,
+                    projectNames[0]);
             dialog.show();
             final int ret = dialog.getSelectedIndex();
             // Execute the action if a project was selected.
@@ -74,10 +78,14 @@ public abstract class LibertyProjectAction extends LibertyGeneralAction {
         }
     }
 
-    public String[] extractBuildFileAttributes(List<BuildFile> buildFileList, Function<BuildFile, String> filter) {
-        return buildFileList.stream()
-                .map(filter)
-                .toArray(String[]::new);
+    public List<Map.Entry<String, String>> extractBuildFileAttributes(List<BuildFile> buildFileList) {
+        List<Map.Entry<String, String>> attributesList = new ArrayList<>();
+        for (BuildFile buildFile : buildFileList) {
+            String projectName = buildFile.getProjectName();
+            String projectPath = buildFile.getBuildFile().getPath();
+            attributesList.add(new AbstractMap.SimpleEntry<>(projectName, projectPath));
+        }
+        return attributesList;
     }
 
     /* Returns an aggregated list containing info for all Maven and Gradle build files. */
