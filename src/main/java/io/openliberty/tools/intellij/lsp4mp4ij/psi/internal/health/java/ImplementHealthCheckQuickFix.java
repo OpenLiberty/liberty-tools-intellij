e663b582b562ca474c59ed40c13c16a02a8a3bf6
vaisakhkannan <133869023+vaisakhkannan@users.noreply.github.com>
2024-08-06 17:19:11 +0530

    Fix Multi catch exception

--- src/main/java/io/openliberty/tools/intellij/lsp4mp4ij/psi/internal/health/java/ImplementHealthCheckQuickFix.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4mp4ij/psi/internal/health/java/ImplementHealthCheckQuickFix.java
@@ -87,7 +87,11 @@ public class ImplementHealthCheckQuickFix implements IJavaCodeActionParticipant 
 					context.getSource().getCompilationUnit());
 			try {
 				toResolve.setEdit(context.convertToWorkspaceEdit(proposal));
-			} catch (IndexNotReadyException | ProcessCanceledException | CancellationException e) {
+			} catch (ProcessCanceledException e) {
+				//Since 2024.2 ProcessCanceledException extends CancellationException so we can't use multicatch to keep backward compatibility
+				//TODO delete block when minimum required version is 2024.2
+				throw e;
+			} catch (IndexNotReadyException | CancellationException e) {
 				throw e;
 			} catch (Exception e) {
 				LOGGER.log(Level.WARNING, "Unable to create workspace edit to make the class implement @HealthCheck", e);
--- src/main/java/io/openliberty/tools/intellij/lsp4mp4ij/psi/internal/metrics/java/ApplicationScopedAnnotationMissingQuickFix.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4mp4ij/psi/internal/metrics/java/ApplicationScopedAnnotationMissingQuickFix.java
@@ -103,7 +103,11 @@ public class ApplicationScopedAnnotationMissingQuickFix implements IJavaCodeActi
 				REMOVE_ANNOTATION_NAMES);
 		try {
 			toResolve.setEdit(context.convertToWorkspaceEdit(proposal));
-		} catch (IndexNotReadyException | ProcessCanceledException | CancellationException e) {
+		} catch (ProcessCanceledException e) {
+			//Since 2024.2 ProcessCanceledException extends CancellationException so we can't use multicatch to keep backward compatibility
+			//TODO delete block when minimum required version is 2024.2
+			throw e;
+		} catch (IndexNotReadyException | CancellationException e) {
 			throw e;
 		} catch (Exception e) {
 			LOGGER.log(Level.WARNING, "Failed to create workspace edit to replace bean scope annotation", e);
