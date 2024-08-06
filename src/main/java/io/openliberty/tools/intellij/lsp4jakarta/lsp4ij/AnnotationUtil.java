aa9d11d49d66bb5fe9aa067d9c08371559d489bc
vaisakhkannan <133869023+vaisakhkannan@users.noreply.github.com>
2024-08-06 19:04:21 +0530

    Updated header year

--- src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/AnnotationUtil.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/AnnotationUtil.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2022 IBM Corporation, Lidia Ataupillco Ramos and others.
+/* Copyright (c) 2022, 2024 IBM Corporation, Lidia Ataupillco Ramos and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
