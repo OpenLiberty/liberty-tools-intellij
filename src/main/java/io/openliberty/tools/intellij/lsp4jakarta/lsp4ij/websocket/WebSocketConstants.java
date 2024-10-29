c4690b3cabe7653e805d400107e1349baacf97d9
Anusree Lakshmi <141303561+anusreelakshmi934@users.noreply.github.com>
2024-10-29 14:37:45 +0530

    Removed unused unqualified names from WebSocketConstants

--- src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/websocket/WebSocketConstants.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/websocket/WebSocketConstants.java
@@ -1,4 +1,4 @@
-/** Copyright (c) 2022, 2023 IBM Corporation and others.
+/** Copyright (c) 2022, 2024 IBM Corporation and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -58,7 +58,6 @@ public class WebSocketConstants {
     public static final String CLIENT_ENDPOINT_ANNOTATION = "jakarta.websocket.ClientEndpoint";
 
     // Superclass
-    public static final String ENDPOINT_SUPERCLASS = "Endpoint";
     public static final String FQ_ENDPOINT_SUPERCLASS = "jakarta.websocket.Endpoint";
     public static final String IS_SUPERCLASS = "isSuperclass";
 
