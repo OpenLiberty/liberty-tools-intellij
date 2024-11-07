0a35dbfae495a3f0719edf868b59627b44a463ba
dessina-devasia <143582034+dessina-devasia@users.noreply.github.com>
2024-11-07 15:14:01 +0530

    Modification in Incorrect Resource Annotation test

--- src/test/resources/projects/maven/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/annotations/IncorrectResourceAnnotation.java
+++ src/test/resources/projects/maven/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/annotations/IncorrectResourceAnnotation.java
@@ -2,36 +2,15 @@ package io.openliberty.sample.jakarta.annotations;
 
 import random.test.pkg.on.Resource;
 
-@Resource(type = Object.class, name = "aa")
 public class IncorrectResourceAnnotation {
 
     private Integer studentId;
 
-
-	@Resource(shareable = true)
-    private boolean isHappy;
-
-	@Resource(name = "test")
+    @Resource
     private boolean isSad;
 
-
-    private String emailAddress;
-
-
-}
-
-@random.test.pkg.on.Resource(name = "aa")
-class PostDoctoralStudent {
-
-    private Integer studentId;
-
-
-	@random.test.pkg.on.Resource(shareable = true)
+    @random.test.pkg.on.Resource
     private boolean isHappy;
-
-	@random.test.pkg.on.Resource
-    private boolean isSad;
-
 
     private String emailAddress;
 
