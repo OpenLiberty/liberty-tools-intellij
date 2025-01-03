3fd8b80e41bc30cf317ef105568e1783e904a02f
dessina-devasia <143582034+dessina-devasia@users.noreply.github.com>
2025-01-03 12:08:16 +0530

    Changed Copyright year to 2025

--- src/main/java/io/openliberty/tools/intellij/lsp4mp4ij/psi/core/ProjectLabelManager.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4mp4ij/psi/core/ProjectLabelManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2019-2020 Red Hat Inc. and others.
+* Copyright (c) 2019-2025 Red Hat Inc. and others.
 * All rights reserved. This program and the accompanying materials
 * which accompanies this distribution, and is available at
 * https://www.eclipse.org/legal/epl-v20.html
