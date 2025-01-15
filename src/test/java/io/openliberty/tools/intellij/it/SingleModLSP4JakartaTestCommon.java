05329ae125f7832491e0e1cac829baea74331db4
dessina-devasia <143582034+dessina-devasia@users.noreply.github.com>
2025-01-15 20:38:30 +0530

    Modification to add automated ui test for completefilterannotationquickfix

---
+++ src/test/java/io/openliberty/tools/intellij/it/SingleModLSP4JakartaTestCommon.java
@@ -0,0 +1,125 @@
+/*******************************************************************************
+ * Copyright (c) 2023, 2024 IBM Corporation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package io.openliberty.tools.intellij.it;
+
+import com.automation.remarks.junit5.Video;
+import com.intellij.remoterobot.RemoteRobot;
+import com.intellij.remoterobot.fixtures.JTreeFixture;
+import io.openliberty.tools.intellij.it.fixtures.ProjectFrameFixture;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+
+import static com.intellij.remoterobot.utils.RepeatUtilsKt.waitForIgnoringError;
+
+public abstract class SingleModLSP4JakartaTestCommon {
+    public static final String REMOTEBOT_URL = "http://localhost:8082";
+    public static final RemoteRobot remoteRobot = new RemoteRobot(REMOTEBOT_URL);
+
+    String projectName;
+    String projectsPath;
+
+    public SingleModLSP4JakartaTestCommon(String projectName, String projectsPath) {
+        this.projectName = projectName;
+        this.projectsPath = projectsPath;
+    }
+
+    /**
+     * Processes actions before each test.
+     *
+     * @param info Test information.
+     */
+    @BeforeEach
+    public void beforeEach(TestInfo info) {
+        TestUtils.printTrace(TestUtils.TraceSevLevel.INFO, this.getClass().getSimpleName() + "." + info.getDisplayName() + ". Entry");
+    }
+
+    /**
+     * Processes actions after each test.
+     *
+     * @param info Test information.
+     */
+    @AfterEach
+    public void afterEach(TestInfo info) {
+        TestUtils.printTrace(TestUtils.TraceSevLevel.INFO, this.getClass().getSimpleName() + "." + info.getDisplayName() + ". Exit");
+        TestUtils.detectFatalError();
+    }
+
+    /**
+     * Cleanup.
+     */
+    @AfterAll
+    public static void cleanup() {
+        if (remoteRobot.isMac()) {
+            UIBotTestUtils.closeAllEditorTabs(remoteRobot);
+        }
+        else {
+            UIBotTestUtils.runActionFromSearchEverywherePanel(remoteRobot, "Close All Tabs", 3);
+        }
+        UIBotTestUtils.closeProjectView(remoteRobot);
+        UIBotTestUtils.closeProjectFrame(remoteRobot);
+        UIBotTestUtils.validateProjectFrameClosed(remoteRobot);
+    }
+
+    @Test
+    @Video
+    public void testCompleteFilterAnnotationQuickFix() {
+        String sourceString = "@WebFilter()";
+        String replacementString = "@WebFilter(servletNames=\"\")";
+        String flaggedString = "WebFilter";
+
+        ProjectFrameFixture projectFrame = remoteRobot.find(ProjectFrameFixture.class, Duration.ofMinutes(2));
+        JTreeFixture projTree = projectFrame.getProjectViewJTree(projectName);
+
+        projTree.expand(projectName, "src", "main", "java", "io.openliberty.sample.jakarta", "servlet");
+        UIBotTestUtils.openFile(remoteRobot, projectName, "InvalidWebFilter", projectName, "src", "main", "java", "io.openliberty.sample.jakarta", "servlet");
+
+        Path pathToSrc = Paths.get(projectsPath, projectName, "src", "main", "java", "io", "openliberty", "sample", "jakarta", "servlet", "InvalidWebFilter.java");
+        String quickfixChooserString = "Add the 'servletNames' attribute to @WebFilter";
+
+        UIBotTestUtils.clickOnFileTab(remoteRobot, "InvalidWebFilter.java");
+        UIBotTestUtils.copyWindowContent(remoteRobot);
+        //UIBotTestUtils.selectAndModifyTextInJavaPart(remoteRobot, "InvalidWebFilter.java", sourceString, replacementString);
+
+        try {
+            //TestUtils.validateStringNotInFile(pathToSrc.toString(), sourceString);
+            UIBotTestUtils.hoverForQuickFixInAppFile(remoteRobot, flaggedString, "InvalidWebFilter.java", quickfixChooserString);
+            UIBotTestUtils.chooseQuickFix(remoteRobot, quickfixChooserString);
+            TestUtils.validateCodeInJavaSrc(pathToSrc.toString(), sourceString);
+        }
+        finally {
+            UIBotTestUtils.pasteOnActiveWindow(remoteRobot);
+        }
+    }
+
+    /**
+     * Prepares the environment to run the tests.
+     *
+     * @param projectPath The path of the project.
+     * @param projectName The name of the project being used.
+     */
+
+    public static void prepareEnv(String projectPath, String projectName) {
+        waitForIgnoringError(Duration.ofMinutes(4), Duration.ofSeconds(5), "Wait for IDE to start", "IDE did not start", () -> remoteRobot.callJs("true"));
+        UIBotTestUtils.findWelcomeFrame(remoteRobot);
+
+        UIBotTestUtils.importProject(remoteRobot, projectPath, projectName);
+        UIBotTestUtils.openProjectView(remoteRobot);
+        // IntelliJ does not start building and indexing until the Project View is open
+        UIBotTestUtils.waitForIndexing(remoteRobot);
+        UIBotTestUtils.openAndValidateLibertyToolWindow(remoteRobot, projectName);
+        UIBotTestUtils.closeLibertyToolWindow(remoteRobot);
+        // Removes the build tool window if it is opened. This prevents text to be hidden by it.
+        UIBotTestUtils.removeToolWindow(remoteRobot, "Build:");
+    }
+}
+
