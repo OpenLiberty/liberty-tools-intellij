8df9f0ac2bca62d45c09db4a61e43824238b5ecb
Joseph-Bineesh <joseph.bineesh@ibm.com>
2025-10-17 14:39:53 +0530

    Test classes for jsonbUnique testcase

---
+++ src/test/resources/projects/maven/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/jsonb/JsonbTransientDiagnosticSubSubClass.java
@@ -0,0 +1,17 @@
+package io.openliberty.sample.jakarta.jsonb;
+
+import jakarta.json.bind.annotation.JsonbProperty;
+
+public class JsonbTransientDiagnosticSubSubClass extends JsonbTransientDiagnosticSubClass{
+	
+
+    @JsonbProperty("name")
+    private String subFirstName;    // Diagnostic: @JsonbProperty property uniqueness in subclass, multiple properties cannot have same property names.
+    
+    @JsonbProperty("fav_editor")
+    private String subfavoriteEditor;    // Diagnostic: @JsonbProperty property uniqueness in subclass, multiple properties cannot have same property names.
+    
+    @JsonbProperty("fav_lang1")
+    private String subfavoriteEditor2;    // Diagnostic: @JsonbProperty property uniqueness in subclass, multiple properties cannot have same property names.
+
+}
