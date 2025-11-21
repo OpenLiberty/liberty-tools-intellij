264f61ab7da2f10420a2d2c43b9501c74b75472a
Anusree Lakshmi <141303561+anusreelakshmi934@users.noreply.github.com>
2025-11-21 10:54:21 +0530

    Updated accessibleName of files in the UI tests (#1461)
    * Update SingleModLibertyLSTestCommon.java
    
    * Update SingleModMPLSTestCommon.java
    
    * Update SingleModMPProjectTestCommon.java
    
    * Update UIBotTestUtils.java
    
    * copyright year update
    
    * editor tab name change revert
    
    * modified equals() to startsWith()
    
    * Update SingleModLibertyLSTestCommon.java
    
    * Update SingleModLibertyLSTestCommon.java
    
    * modified to use starts-with()
    
    * Update SingleModLibertyLSTestCommon.java
    
    Revert "Update SingleModLibertyLSTestCommon.java"
    
    This reverts commit e678a1a29d99aac0186398c6433151be92828b84.
    
    Update SingleModMPLSTestCommon.java

--- src/test/java/io/openliberty/tools/intellij/it/UIBotTestUtils.java
+++ src/test/java/io/openliberty/tools/intellij/it/UIBotTestUtils.java
@@ -744,7 +744,7 @@ public class UIBotTestUtils {
         ProjectFrameFixture projectFrame = remoteRobot.find(ProjectFrameFixture.class, Duration.ofSeconds(10));
 
         try {
-            String xPath = "//div[@accessiblename='" + fileName + "' and @class='EditorTabLabel']";
+            String xPath = "//div[starts-with(@accessiblename, '" + fileName + "') and @class='EditorTabLabel']";
             ComponentFixture actionButton = projectFrame.getActionButton(xPath, "10");
             actionButton.click();
 
@@ -780,7 +780,7 @@ public class UIBotTestUtils {
                 // Find the target text on the editor and move the move to it.
                 editorNew.findText(contains(hoverTarget)).moveMouse();
                 // clear and "lightbulb" icons?
-                if (!hoverFile.equals("server.xml")) {
+                if (!hoverFile.startsWith("server.xml")) {
                     keyboard.hotKey(VK_ESCAPE);
                 }
 
@@ -2069,6 +2069,11 @@ public class UIBotTestUtils {
             ActionButtonFixture addCfgButton = addProjectDialog.actionButton(addButtonLocator);
             addCfgButton.click();
 
+            // Click on the Collapse All button.
+            Locator collapseButtonLocator = byXpath("//div[@accessiblename='Collapse All']");
+            ActionButtonFixture collapseButton = addProjectDialog.actionButton(collapseButtonLocator);
+            collapseButton.click();
+
             // Look for the Liberty entry in the Add New configuration window and  create a new configuration.
             ComponentFixture pluginCfgTree = addProjectDialog.getMyTree();
             RepeatUtilsKt.waitFor(Duration.ofSeconds(10),
@@ -2400,7 +2405,7 @@ public class UIBotTestUtils {
     public static void runConfigUsingIconOnToolbar(RemoteRobot remoteRobot, ExecMode execMode) {
         ProjectFrameFixture projectFrame = remoteRobot.find(ProjectFrameFixture.class, Duration.ofSeconds(10));
 
-        Locator locator = byXpath("//div[@class='ActionButton' and @myaction='Run (Run selected configuration)']");
+        Locator locator = byXpath("//div[@class='ActionButton' and @myaction='Run (Run the selected configuration)']");
         if (execMode == ExecMode.DEBUG) {
             locator = byXpath("//div[@class='ActionButton' and @myicon='debug.svg']");
         }
