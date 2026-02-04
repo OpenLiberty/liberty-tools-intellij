b7f2773a8270011889caad5b3dd3177b8c2cc4c0
Archana Iyer <archanr@ibm.com>
2026-02-04 15:14:24 +0530

    Added copyrights

--- src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/di/DIUtils.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/di/DIUtils.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation, Archana Iyer - initial API and implementation
+ *******************************************************************************/
 package io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.di;
 
 import com.intellij.psi.*;
