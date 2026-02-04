50c8704f0f8aa31e6dd34d34ade87fccae8b37b3
Rejoy <rejoy.kulangara@ibm.com>
2026-02-04 10:06:46 +0530

    Validate-@Resource-type-compatibility - added utility methods

--- src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/DiagnosticsUtils.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/DiagnosticsUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 IBM Corporation
+ * Copyright (c) 2025, 2026 IBM Corporation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -22,9 +22,12 @@ import com.intellij.psi.search.GlobalSearchScope;
  */
 public class DiagnosticsUtils {
 
+    public static final String NAME_MUST_START_WITH_SET = "NameMustStartWithSet";
+    public static final String MUST_DECLARE_EXACTLY_ONE_PARAM = "MustDeclareExactlyOneParam";
+    public static final String RETURN_TYPE_MUST_BE_VOID = "ReturnTypeMustBeVoid";
     /**
      * inheritsFrom
-     * Check if specified superType is present or not in the type hierarchy
+     * find super class and Check if it is present or not in the type hierarchy
      *
      * @param clazz
      * @param fqSuperType
@@ -34,7 +37,42 @@ public class DiagnosticsUtils {
         Project project = clazz.getProject();
         PsiClass superClass = JavaPsiFacade.getInstance(project)
                 .findClass(fqSuperType, GlobalSearchScope.allScope(project));
-        return superClass != null &&
-                (clazz.isEquivalentTo(superClass) || clazz.isInheritor(superClass, true));
+        return inheritsFrom(clazz, superClass);
+    }
+
+    /**
+     * inheritsFrom
+     * Check if specified superClass is present or not in the type hierarchy
+     *
+     * @param clazz
+     * @param superClass
+     * @return
+     */
+    public static boolean inheritsFrom(PsiClass clazz, PsiClass superClass) {
+        if (clazz == null || superClass == null) {
+            return false;
+        }
+        return clazz.isEquivalentTo(superClass) || clazz.isInheritor(superClass, true);
+    }
+
+
+    /**
+     * validateSetterMethod
+     * This is to check whether a method is a valid setter.
+     *
+     * @param element
+     * @return
+     */
+    public static String validateSetterMethod(PsiMethod element) {
+        String methodName = element.getName();
+        PsiType returnType = element.getReturnType();
+        if(!methodName.startsWith("set")){
+            return NAME_MUST_START_WITH_SET;
+        } else if(!(returnType == null || returnType.equals(PsiTypes.voidType()))){
+            return RETURN_TYPE_MUST_BE_VOID;
+        } else if(element.getParameterList().getParametersCount() != 1){
+            return MUST_DECLARE_EXACTLY_ONE_PARAM;
+        }
+        return null;
     }
 }
