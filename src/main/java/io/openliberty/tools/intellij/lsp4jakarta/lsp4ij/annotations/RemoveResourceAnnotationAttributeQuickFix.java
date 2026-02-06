c79538edad9ed76b489d5af9de9e2bbc00a9084c
Rejoy <rejoy.kulangara@ibm.com>
2026-02-06 17:29:34 +0530

    created 2 new quick fixes

---
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/annotations/RemoveResourceAnnotationAttributeQuickFix.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+* Copyright (c) 2026 IBM Corporation and others.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v. 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*     IBM Corporation - initial implementation
+*******************************************************************************/
+
+package io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.annotations;
+
+
+import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.codeAction.proposal.quickfix.RemoveAnnotationAttributesQuickFix;
+
+import org.eclipse.lsp4jakarta.jdt.internal.Messages;
+
+/**
+ * Removes the @Resource annotation from the declaring element.
+ */
+public class RemoveResourceAnnotationAttributeQuickFix extends RemoveAnnotationAttributesQuickFix {
+
+    public RemoveResourceAnnotationAttributeQuickFix() {
+        super("jakarta.annotation.Resource","type");
+    }
+
+    @Override
+    public String getLabel() {
+        return "df";
+    }
+
+    @Override
+    public String getParticipantId() {
+        return RemoveResourceAnnotationAttributeQuickFix.class.getName();
+    }
+}
