8bbbdd196f389c2ab461a4a924e50bd7e1d3d302
Rejoy <rejoy.kulangara@ibm.com>
2026-03-04 17:41:14 +0530

    feature/DeclareRoles-on-Non-Servlet-Classes - tests

---
+++ src/test/resources/projects/maven/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/servlet/DeclareRolesWithoutServlet.java
@@ -0,0 +1,8 @@
+package io.openliberty.sample.jakarta.servlet;
+
+import jakarta.annotation.security.DeclareRoles;
+
+@DeclareRoles("Administrator")
+public class DeclareRolesWithoutServlet {
+    
+}
