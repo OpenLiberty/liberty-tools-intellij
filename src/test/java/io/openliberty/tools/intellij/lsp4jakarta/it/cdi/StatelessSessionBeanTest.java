fc73eed371e48a9ac31652adb9f15c8b502a7ed3
Rejoy <rejoy.kulangara@ibm.com>
2026-03-27 12:23:21 +0530

    Stateless-session-beans-must-be-annotated-with-@Dependent - tests

---
+++ src/test/java/io/openliberty/tools/intellij/lsp4jakarta/it/cdi/StatelessSessionBeanTest.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2021, 2026 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package io.openliberty.tools.intellij.lsp4jakarta.it.cdi;
+
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.openapi.vfs.VirtualFile;
+import io.openliberty.tools.intellij.lsp4jakarta.it.core.BaseJakartaTest;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.utils.IPsiUtils;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.internal.core.ls.PsiUtilsLSImpl;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.eclipse.lsp4jakarta.commons.JakartaJavaDiagnosticsParams;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.File;
+import java.util.Arrays;
+
+import static io.openliberty.tools.intellij.lsp4jakarta.it.core.JakartaForJavaAssert.*;
+
+@RunWith(JUnit4.class)
+public class StatelessSessionBeanTest extends BaseJakartaTest {
+
+    @Test
+    public void statelessSessionBeanWithIllegalScope() throws Exception {
+        Module module = createMavenModule(new File("src/test/resources/projects/maven/jakarta-sample"));
+        IPsiUtils utils = PsiUtilsLSImpl.getInstance(getProject());
+
+        VirtualFile javaFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(ModuleUtilCore.getModuleDirPath(module)
+                + "/src/main/java/io/openliberty/sample/jakarta/cdi/StatelessSessionBean.java");
+        String uri = VfsUtilCore.virtualToIoFile(javaFile).toURI().toString();
+
+        JakartaJavaDiagnosticsParams diagnosticsParams = new JakartaJavaDiagnosticsParams();
+        diagnosticsParams.setUris(Arrays.asList(uri));
+
+        // Test expected diagnostics
+        Diagnostic d1 = d(10, 13, 39,
+                "A stateless session bean belongs to the @Dependent scope; any other scope is invalid.",
+                DiagnosticSeverity.Error, "jakarta-cdi", "InvalidStatelessSessionBeanWithIllegalScope");
+
+        Diagnostic d2 = d(16, 6, 32,
+                "A stateless session bean belongs to the @Dependent scope; any other scope is invalid.",
+                DiagnosticSeverity.Error, "jakarta-cdi", "InvalidStatelessSessionBeanWithIllegalScope");
+
+        Diagnostic d3 = d(24, 6, 33,
+                "A stateless session bean belongs to the @Dependent scope; any other scope is invalid.",
+                DiagnosticSeverity.Error, "jakarta-cdi", "InvalidStatelessSessionBeanWithIllegalScope");
+
+        assertJavaDiagnostics(diagnosticsParams, utils, d1, d2, d3);
+    }
+}
---
+++ src/test/resources/projects/maven/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/cdi/StatelessSessionBean.java
@@ -0,0 +1,36 @@
+package io.openliberty.sample.jakarta.cdi;
+
+import jakarta.ejb.Stateless;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.enterprise.context.Dependent;
+
+// Invalid: Stateless with RequestScoped
+@Stateless
+@RequestScoped
+public class StatelessSessionBean {
+}
+
+// Invalid: Stateless with SessionScoped
+@Stateless
+@SessionScoped
+class StatelessWithSessionScoped {
+}
+
+// Invalid: Stateless with multiple scopes including Dependent
+@Stateless
+@Dependent
+@RequestScoped
+class StatelessWithMultipleScopes {
+}
+
+// Valid: Stateless with no explicit scope (defaults to @Dependent)
+@Stateless
+class StatelessWithNoScope {
+}
+
+// Valid: Stateless with only Dependent
+@Stateless
+@Dependent
+class StatelessWithDependent {
+}
