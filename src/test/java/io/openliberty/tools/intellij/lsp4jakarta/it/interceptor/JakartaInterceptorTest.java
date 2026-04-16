a86539000a7046d810bbefbf8114a5a0b8c1aa61
Archana Iyer <archanr@ibm.com>
2026-04-16 15:49:01 +0530

    Refactored test case

--- src/test/java/io/openliberty/tools/intellij/lsp4jakarta/it/interceptor/JakartaInterceptorTest.java
+++ src/test/java/io/openliberty/tools/intellij/lsp4jakarta/it/interceptor/JakartaInterceptorTest.java
@@ -36,43 +36,42 @@ public class JakartaInterceptorTest extends BaseJakartaTest {
 
     @Test
     public void invalidInterceptorTest() throws Exception {
-      Module module = createMavenModule(new File("src/test/resources/projects/maven/jakarta-sample"));
-      IPsiUtils utils = PsiUtilsLSImpl.getInstance(getProject());
+        Module module = createMavenModule(new File("src/test/resources/projects/maven/jakarta-sample"));
+        IPsiUtils utils = PsiUtilsLSImpl.getInstance(getProject());
 
-      VirtualFile javaFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(ModuleUtilCore.getModuleDirPath(module)
-          + "/src/main/java/io/openliberty/sample/jakarta/interceptor/InvalidInterceptor.java");
-      String uri = VfsUtilCore.virtualToIoFile(javaFile).toURI().toString();
+        VirtualFile javaFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(ModuleUtilCore.getModuleDirPath(module)
+                + "/src/main/java/io/openliberty/sample/jakarta/interceptor/InvalidInterceptor.java");
+        String uri = VfsUtilCore.virtualToIoFile(javaFile).toURI().toString();
 
-      JakartaJavaDiagnosticsParams diagnosticsParams = new JakartaJavaDiagnosticsParams();
-      diagnosticsParams.setUris(Arrays.asList(uri));
+        JakartaJavaDiagnosticsParams diagnosticsParams = new JakartaJavaDiagnosticsParams();
+        diagnosticsParams.setUris(Arrays.asList(uri));
 
-      // Test diagnostics
-      Diagnostic d1 = JakartaForJavaAssert.d(5, 13, 31,
-          "Missing Public NoArgsConstructor. Class InvalidInterceptor is of Interceptor type, but does not declare a public no-argument constructor.",
-          DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorAnnotationOnNoArgsConstructor");
-      Diagnostic d2 = JakartaForJavaAssert.d(32, 14, 37,
-          "Missing Public NoArgsConstructor. Class InnerInvalidInterceptor is of Interceptor type, but does not declare a public no-argument constructor.",
-          DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorAnnotationOnNoArgsConstructor");
+        // Test diagnostics
+        Diagnostic d1 = JakartaForJavaAssert.d(5, 13, 31,
+                "Missing Public NoArgsConstructor. Class InvalidInterceptor is of Interceptor type, but does not declare a public no-argument constructor.",
+                DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorAnnotationOnNoArgsConstructor");
+        Diagnostic d2 = JakartaForJavaAssert.d(32, 14, 37,
+                "Missing Public NoArgsConstructor. Class InnerInvalidInterceptor is of Interceptor type, but does not declare a public no-argument constructor.",
+                DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorAnnotationOnNoArgsConstructor");
 
-      JakartaForJavaAssert.assertJavaDiagnostics(diagnosticsParams, utils, d1, d2);
+        JakartaForJavaAssert.assertJavaDiagnostics(diagnosticsParams, utils, d1, d2);
     }
-	
     @Test
     public void invalidAbstractInterceptorTest() throws Exception {
-      Module module = createMavenModule(new File("src/test/resources/projects/maven/jakarta-sample"));
-      IPsiUtils utils = PsiUtilsLSImpl.getInstance(getProject());
+        Module module = createMavenModule(new File("src/test/resources/projects/maven/jakarta-sample"));
+        IPsiUtils utils = PsiUtilsLSImpl.getInstance(getProject());
 
-      VirtualFile javaFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(ModuleUtilCore.getModuleDirPath(module)
-          + "/src/main/java/io/openliberty/sample/jakarta/interceptor/InvalidAbstractInterceptor.java");
-      String uri = VfsUtilCore.virtualToIoFile(javaFile).toURI().toString();
+        VirtualFile javaFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(ModuleUtilCore.getModuleDirPath(module)
+                + "/src/main/java/io/openliberty/sample/jakarta/interceptor/InvalidAbstractInterceptor.java");
+        String uri = VfsUtilCore.virtualToIoFile(javaFile).toURI().toString();
 
-      JakartaJavaDiagnosticsParams diagnosticsParams = new JakartaJavaDiagnosticsParams();
-      diagnosticsParams.setUris(Arrays.asList(uri));
+        JakartaJavaDiagnosticsParams diagnosticsParams = new JakartaJavaDiagnosticsParams();
+        diagnosticsParams.setUris(Arrays.asList(uri));
 
-      // Test diagnostics
-      Diagnostic d1 = JakartaForJavaAssert.d(5, 22, 40,
-          "The class InvalidInterceptor should not contain the abstract modifier. If it contains the abstract modifier, the class should not be annotated with @Interceptor.",
-          DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorAnnotationOnAbstractClass");
+        // Test diagnostics
+        Diagnostic d1 = JakartaForJavaAssert.d(5, 22, 40,
+                "The class InvalidInterceptor should not contain the abstract modifier. If it contains the abstract modifier, the class should not be annotated with @Interceptor.",
+                DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorAnnotationOnAbstractClass");
     }
 
     @Test
@@ -88,31 +87,33 @@ public class JakartaInterceptorTest extends BaseJakartaTest {
         diagnosticsParams.setUris(Arrays.asList(uri));
 
         // Test diagnostics
-        Diagnostic d1 = JakartaForJavaAssert.d(12, 18, 38,
+        Diagnostic aroundInvokeInvalidProceed = JakartaForJavaAssert.d(12, 18, 38,
                 "Interceptor methods must always call the InvocationContext.proceed method.",
                 DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorMethodAnnotationOnMethod");
-        Diagnostic d2 = JakartaForJavaAssert.d(18, 18, 41,
+        Diagnostic aroundConstructInvalidProceed = JakartaForJavaAssert.d(18, 18, 41,
                 "Interceptor methods must always call the InvocationContext.proceed method.",
                 DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorMethodAnnotationOnMethod");
-        Diagnostic d3 = JakartaForJavaAssert.d(24, 18, 39,
+        Diagnostic aroundTimeoutInvalidProceed = JakartaForJavaAssert.d(24, 18, 39,
                 "Interceptor methods must always call the InvocationContext.proceed method.",
                 DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorMethodAnnotationOnMethod");
-        Diagnostic d4 = JakartaForJavaAssert.d(30, 16, 36,
+        Diagnostic postConstructInvalidProceed = JakartaForJavaAssert.d(30, 16, 36,
                 "Interceptor methods must always call the InvocationContext.proceed method.",
                 DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorMethodAnnotationOnMethod");
-        Diagnostic d5 = JakartaForJavaAssert.d(35, 16, 33,
+        Diagnostic preDestroyInvalidProceed = JakartaForJavaAssert.d(35, 16, 33,
                 "Interceptor methods must always call the InvocationContext.proceed method.",
                 DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorMethodAnnotationOnMethod");
-        Diagnostic d6 = JakartaForJavaAssert.d(46, 19, 44,
+        Diagnostic aroundInvokeInvalidProceedChild = JakartaForJavaAssert.d(46, 19, 44,
                 "Interceptor methods must always call the InvocationContext.proceed method.",
                 DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorMethodAnnotationOnMethod");
-        Diagnostic d7 = JakartaForJavaAssert.d(62, 17, 42,
+        Diagnostic postConstructInvalidProceedChild = JakartaForJavaAssert.d(62, 17, 42,
                 "Interceptor methods must always call the InvocationContext.proceed method.",
                 DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorMethodAnnotationOnMethod");
-        Diagnostic d8 = JakartaForJavaAssert.d(67, 14, 36,
+        Diagnostic preDestroyInvalidProceedChild = JakartaForJavaAssert.d(67, 14, 36,
                 "Interceptor methods must always call the InvocationContext.proceed method.",
                 DiagnosticSeverity.Error, "jakarta-interceptor", "RemoveInterceptorMethodAnnotationOnMethod");
 
-        JakartaForJavaAssert.assertJavaDiagnostics(diagnosticsParams, utils, d1, d2, d3, d4, d5, d6, d7, d8);
+        JakartaForJavaAssert.assertJavaDiagnostics(diagnosticsParams, utils, aroundInvokeInvalidProceed, aroundConstructInvalidProceed,
+                aroundTimeoutInvalidProceed, postConstructInvalidProceed, preDestroyInvalidProceed, aroundInvokeInvalidProceedChild,
+                postConstructInvalidProceedChild, preDestroyInvalidProceedChild);
     }
 }
