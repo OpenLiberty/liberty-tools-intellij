1428c5a1efa75551dc6836ee4b69e34d4775aa4d
Sajeer <sajeer.babu@ibm.com>
2026-06-04 16:35:48 +0530

    Added diagnostic changes for EJB No-Args constructor validation

---
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/ejb/InsertPublicNoArgConstructorQuickFix.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.ejb;
+
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.JDTUtils;
+import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.Messages;
+import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.codeAction.proposal.AddConstructorProposal;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.codeaction.IJavaCodeActionParticipant;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.codeaction.JavaCodeActionContext;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.codeaction.JavaCodeActionResolveContext;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.corrections.proposal.ChangeCorrectionProposal;
+import io.openliberty.tools.intellij.util.ExceptionUtil;
+import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.Diagnostic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * QuickFix for adding a public no-arg constructor to session beans.
+ */
+public class InsertPublicNoArgConstructorQuickFix implements IJavaCodeActionParticipant {
+    private static final Logger LOGGER = Logger.getLogger(InsertPublicNoArgConstructorQuickFix.class.getName());
+
+    @Override
+    public String getParticipantId() {
+        return InsertPublicNoArgConstructorQuickFix.class.getName();
+    }
+
+    @Override
+    public List<? extends CodeAction> getCodeActions(JavaCodeActionContext context, Diagnostic diagnostic) {
+        List<CodeAction> codeActions = new ArrayList<>();
+        PsiElement node = context.getCoveredNode();
+        PsiClass parentType = getBinding(node);
+
+        if (parentType != null) {
+            String constructorName = Messages.getMessage("AddPublicNoArgConstructor");
+            CodeAction codeAction = JDTUtils.createCodeAction(context, diagnostic, constructorName, getParticipantId());
+            codeActions.add(codeAction);
+        }
+        return codeActions;
+    }
+
+    @Override
+    public CodeAction resolveCodeAction(JavaCodeActionResolveContext context) {
+        final CodeAction toResolve = context.getUnresolved();
+        PsiElement node = context.getCoveredNode();
+        PsiClass parentType = getBinding(node);
+
+        String constructorName = toResolve.getTitle();
+        ChangeCorrectionProposal proposal = new AddConstructorProposal(
+                constructorName,
+                context.getSource().getCompilationUnit(),
+                context.getASTRoot(),
+                parentType,
+                0,
+                "public");
+
+        ExceptionUtil.executeWithWorkspaceEditHandling(
+                context,
+                proposal,
+                toResolve,
+                LOGGER,
+                "Unable to create workspace edit for code action to add public no-arg constructor");
+        return toResolve;
+    }
+
+    protected PsiClass getBinding(PsiElement node) {
+        return PsiTreeUtil.getParentOfType(node, PsiClass.class);
+    }
+}
