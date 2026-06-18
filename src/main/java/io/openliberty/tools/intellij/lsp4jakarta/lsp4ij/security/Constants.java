3bfadcf98e9285ac0014116d419e48a2fdcc81c6
Arun Venmany <arun.kumar.v.n@ibm.com>
2026-06-18 12:46:57 +0530

    Diagnostics for @LdapIdentityStoreDefinition and @DatabaseIdentityStoreDefinition qualifiers and scope

---
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/security/Constants.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial implementation
+ *******************************************************************************/
+package io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.security;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Jakarta Security constants.
+ */
+public class Constants {
+
+    /* Identity Store Definition Annotations */
+    public static final String LDAP_IDENTITY_STORE_DEFINITION_FQ_NAME = "jakarta.security.enterprise.identitystore.LdapIdentityStoreDefinition";
+    public static final String DATABASE_IDENTITY_STORE_DEFINITION_FQ_NAME = "jakarta.security.enterprise.identitystore.DatabaseIdentityStoreDefinition";
+    
+    /* CDI Scope Annotation */
+    public static final String APPLICATION_SCOPED_FQ_NAME = "jakarta.enterprise.context.ApplicationScoped";
+    
+    /* CDI Scope Annotations */
+    public static final Set<String> SCOPE_FQ_NAMES = new HashSet<>(Arrays.asList(
+        "jakarta.enterprise.context.Dependent",
+        "jakarta.enterprise.context.ApplicationScoped",
+        "jakarta.enterprise.context.ConversationScoped",
+        "jakarta.enterprise.context.RequestScoped",
+        "jakarta.enterprise.context.SessionScoped",
+        "jakarta.enterprise.context.NormalScope",
+        "jakarta.Interceptor",
+        "jakarta.Decorator",
+        "jakarta.enterprise.inject.Stereotype"
+    ));
+    
+    /* Diagnostic Source */
+    public static final String DIAGNOSTIC_SOURCE = "jakarta-security";
+    
+    /* Diagnostic Codes */
+    public static final String DIAGNOSTIC_CODE_MISSING_APPLICATION_SCOPED = "MissingApplicationScopedOnIdentityStoreDefinition";
+    public static final String DIAGNOSTIC_CODE_INVALID_SCOPE = "InvalidScopeOnIdentityStoreDefinition";
+}
