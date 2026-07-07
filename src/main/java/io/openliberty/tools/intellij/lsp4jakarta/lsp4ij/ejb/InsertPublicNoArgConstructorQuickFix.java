695cc10c99545f0e3c28d117d98eace919ffa7cc
Sajeer <sajeer.babu@ibm.com>
2026-07-07 14:25:07 +0530

    Revert out-of-scope refactoring of InsertPublicNoArgConstructorQuickFix

--- src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/ejb/InsertPublicNoArgConstructorQuickFix.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/ejb/InsertPublicNoArgConstructorQuickFix.java
@@ -13,30 +13,20 @@
 
 package io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.ejb;
 
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.PsiTreeUtil;
 import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.JDTUtils;
 import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.Messages;
-import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.codeAction.proposal.AddConstructorProposal;
-import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.codeaction.IJavaCodeActionParticipant;
+import io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.codeAction.common.InsertDefaultPubOrProConstructorQuickFix;
 import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.codeaction.JavaCodeActionContext;
-import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.codeaction.JavaCodeActionResolveContext;
-import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.java.corrections.proposal.ChangeCorrectionProposal;
-import io.openliberty.tools.intellij.util.ExceptionUtil;
 import org.eclipse.lsp4j.CodeAction;
 import org.eclipse.lsp4j.Diagnostic;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * QuickFix for adding a public no-arg constructor to session beans.
  */
-public class InsertPublicNoArgConstructorQuickFix implements IJavaCodeActionParticipant {
-
-    private static final Logger LOGGER = Logger.getLogger(InsertPublicNoArgConstructorQuickFix.class.getName());
+public class InsertPublicNoArgConstructorQuickFix extends InsertDefaultPubOrProConstructorQuickFix {
 
     @Override
     public String getParticipantId() {
@@ -45,29 +35,11 @@ public class InsertPublicNoArgConstructorQuickFix implements IJavaCodeActionPart
 
     @Override
     public List<? extends CodeAction> getCodeActions(JavaCodeActionContext context, Diagnostic diagnostic) {
-        PsiElement node = context.getCoveredNode();
-        PsiClass parentType = getBinding(node);
-        if (parentType != null) {
-            String constructorName = Messages.getMessage("AddPublicConstructor");
-            return List.of(JDTUtils.createCodeAction(context, diagnostic, constructorName, getParticipantId()));
+        List<CodeAction> codeActions = new ArrayList<>();
+        if (getBinding(context.getCoveredNode()) != null) {
+            String constructorName = Messages.getMessage("AddPublicNoArgConstructor");
+            codeActions.add(JDTUtils.createCodeAction(context, diagnostic, constructorName, getParticipantId()));
         }
-        return Collections.emptyList();
-    }
-
-    @Override
-    public CodeAction resolveCodeAction(JavaCodeActionResolveContext context) {
-        final CodeAction toResolve = context.getUnresolved();
-        PsiElement node = context.getCoveredNode();
-        PsiClass parentType = getBinding(node);
-        String constructorName = toResolve.getTitle();
-        ChangeCorrectionProposal proposal = new AddConstructorProposal(constructorName,
-                context.getSource().getCompilationUnit(), context.getASTRoot(), parentType, 0, "public");
-        ExceptionUtil.executeWithWorkspaceEditHandling(context, proposal, toResolve, LOGGER,
-                "Unable to create workspace edit for code action to add constructor");
-        return toResolve;
-    }
-
-    protected PsiClass getBinding(PsiElement node) {
-        return PsiTreeUtil.getParentOfType(node, PsiClass.class);
+        return codeActions;
     }
 }
