74bee4e6e572679e2b1d857f5fb1450da76c8cb4
Arunvenmany <arunvenmany@gmail.com>
2026-07-21 08:54:27 +0530

    ejb message driven bean MessageListener interface diagnostic and quickfix (#1620)
    * ejb message driven bean MessageListener interface diagnostic and quickfixe
    
    * changes based on review comments
    
    * changes based on review comments
    
    * changing diagnostic code constant based on review comments

--- src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/ejb/EjbConstants.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4jakarta/lsp4ij/ejb/EjbConstants.java
@@ -14,20 +14,26 @@
 package io.openliberty.tools.intellij.lsp4jakarta.lsp4ij.ejb;
 
 /**
- * EJB constants
+ * Constants for Jakarta Enterprise Beans (EJB) diagnostics.
  */
 public class EjbConstants {
+    /* Annotations */
+    public static final String MESSAGE_DRIVEN_FQ_NAME = "jakarta.ejb.MessageDriven";
     public static final String STATELESS_FQ_NAME = "jakarta.ejb.Stateless";
     public static final String STATEFUL_FQ_NAME = "jakarta.ejb.Stateful";
     public static final String SINGLETON_FQ_NAME = "jakarta.ejb.Singleton";
 
+    /* Interfaces */
+    public static final String MESSAGE_LISTENER_FQ_NAME = "jakarta.jms.MessageListener";
     public static final String[] SESSION_BEAN_ANNOTATIONS = {
             STATELESS_FQ_NAME,
             STATEFUL_FQ_NAME,
             SINGLETON_FQ_NAME
     };
 
+    /* Diagnostic codes */
     public static final String DIAGNOSTIC_SOURCE = "jakarta-ejb";
+    public static final String DIAGNOSTIC_CODE_MISSING_MESSAGELISTENER = "ImplementMessageListener";
     public static final String DIAGNOSTIC_CODE_MISSING_PUBLIC_CONSTRUCTOR = "MissingPublicNoArgConstructor";
     public static final String DIAGNOSTIC_CODE_FINALIZE_METHOD = "SessionBeanFinalizeMethod";
     
